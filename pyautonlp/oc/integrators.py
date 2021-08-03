6af84ec0ec79c09a11d17c192519d8ba846af3af
core_bit <nkorbit@outlook.com>
2021-08-03 17:27:49 +0300

    Adaptive EEuler method.

--- pyautonlp/oc/integrators.py
+++ pyautonlp/oc/integrators.py
@@ -1,13 +1,22 @@
+from typing import Callable
+
+import matplotlib.pyplot as plt
 import jax.numpy as jnp
 
 from pyautonlp.constants import IntegrateMethod
 
 
-def integrate(fn, x0, u0, time_grid, method=IntegrateMethod.RK4):
+def integrate(
+        fn: Callable,
+        x0: jnp.ndarray,
+        u0: jnp.ndarray,
+        time_grid: jnp.ndarray,
+        method: str = IntegrateMethod.RK4,
+) -> jnp.ndarray:
     if method == IntegrateMethod.EEULER:
         return _eeuler(fn, x0, u0, time_grid)
     elif method == IntegrateMethod.SSC_EEULER:
-        return _eeuler(fn, x0, u0, time_grid, adaptive=True)
+        return _eeuler_adaptive(fn, x0, u0, time_grid)
     elif method == IntegrateMethod.RK4:
         return _erk4(fn, x0, u0, time_grid)
     elif method == IntegrateMethod.IEULER:
@@ -16,21 +25,22 @@ def integrate(fn, x0, u0, time_grid, method=IntegrateMethod.RK4):
         raise ValueError(f'Unrecognized integration method: {method}.')
 
 
-def _eeuler(fn, x0, u0, time_grid, adaptive=False) -> jnp.ndarray:
+def _eeuler(
+        fn: Callable,
+        x0: jnp.ndarray,
+        u0: jnp.ndarray,
+        time_grid: jnp.ndarray,
+) -> jnp.ndarray:
     res = [x0]
     x_i = x0
     for t_i, t_j in zip(time_grid[0:-1], time_grid[1:]):
         h = t_j - t_i
 
-        # x'(t)
+        # dx/dt - slope
         s_i = fn(x_i, u0, t_i)
 
         # new state
-        if not adaptive:
-            x_j = x_i + h * s_i
-        else:
-            x_j = x_i + h * s_i
-
+        x_j = x_i + h * s_i
         res.append(x_j)
 
         # update vars
@@ -39,7 +49,59 @@ def _eeuler(fn, x0, u0, time_grid, adaptive=False) -> jnp.ndarray:
     return jnp.stack(res)
 
 
-def _erk4(fn, x0, u0, time_grid) -> jnp.ndarray:
+def _eeuler_adaptive(
+        fn: Callable,
+        x0: jnp.ndarray,
+        u0: jnp.ndarray,
+        time_grid: jnp.ndarray,
+        t_rel: float = 0.,
+        t_abs: float = 1.,
+        plot_h: bool = False,
+) -> jnp.ndarray:
+    res = [x0]
+    x_i = x0
+    t = time_grid[0]
+    h = time_grid[1] - time_grid[0]
+    tn = time_grid[-1]
+    s_i = fn(x_i, u0, t)
+
+    h_series = [h]
+
+    while not jnp.isclose(t, tn):
+        # correction for last time step
+        if t + h > tn:
+            h = tn - t
+
+        x_j = x_i + h * s_i
+        s_j = fn(x_j, u0, t + h)
+
+        e = jnp.linalg.norm(s_j - s_i, ord=2) / 2
+        # TODO t_rel, t_abs
+
+        # make a step if error is small
+        if e <= 1.:
+            res.append(x_j)
+            x_i = x_j
+            s_i = s_j
+            t += h
+
+        # update interval h
+        h *= jnp.minimum(2., jnp.maximum(.5, .9 / jnp.sqrt(e)))
+        h_series.append(h)
+
+    if plot_h:
+        plt.plot(h_series)
+        plt.show()
+
+    return jnp.stack(res)
+
+
+def _erk4(
+        fn: Callable,
+        x0: jnp.ndarray,
+        u0: jnp.ndarray,
+        time_grid: jnp.ndarray,
+) -> jnp.ndarray:
     res = [x0]
     x_i = x0
     for t_i, t_j in zip(time_grid[0:-1], time_grid[1:]):
@@ -61,7 +123,12 @@ def _erk4(fn, x0, u0, time_grid) -> jnp.ndarray:
     return jnp.stack(res)
 
 
-def _ieuler(fn, x0, u0, time_grid) -> jnp.ndarray:
+def _ieuler(
+        fn: Callable,
+        x0: jnp.ndarray,
+        u0: jnp.ndarray,
+        time_grid: jnp.ndarray,
+) -> jnp.ndarray:
     res = [x0]
     x_i = x0
     for t_i, t_j in zip(time_grid[0:-1], time_grid[1:]):
@@ -77,5 +144,4 @@ def _ieuler(fn, x0, u0, time_grid) -> jnp.ndarray:
         # update vars
         x_i = x_j
 
-    # return jnp.array(res, dtype=jnp.float32)
     return jnp.stack(res)
