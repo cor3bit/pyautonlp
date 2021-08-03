edffe65d36c5a340109f24c3259e2240fb60d200
core_bit <nkorbit@outlook.com>
2021-08-03 12:49:50 +0300

    Adds packaging.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import setuptools
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setuptools.setup(
+    name='PyAutoNLP',
+    version='0.1',
+    author='Nick Korbit',
+    description="JAX Implementation of Differentiable NLP Solvers.",
+    long_description=read('README.md'),
+    packages=setuptools.find_packages(exclude=['examples', 'tests']),
+    install_requires=[
+        'jax',
+    ],
+)
